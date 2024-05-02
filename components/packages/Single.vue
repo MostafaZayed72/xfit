d0860d8c9c243457f1e295b9429af37890252939
MostafaZayed72 <zayed8646@gmail.com>
2024-05-02 16:53:12 +0300

    feat : add pay methods

--- components/packages/Single.vue
+++ components/packages/Single.vue
@@ -2,13 +2,49 @@
     <main>
      
       <div class="row flex gap-5 flex-wrap justify-center ">
+         <!-- IF LARGE SCREEN -->
          <div
-            class="col-sm-6 col-md-4 mb-4  rounded  sm:w-100 lg:w-1/3 main-card"
+            class="col-sm-6 col-md-4 mb-4  rounded hidden lg:block   w-1/3 main-card"
             v-for="the_package in packages?.data"
             :key="the_package.id"
          >
             <div
-               class="h-100 shadow-sm bg-light rounded-3 d-block p-4 text-center"
+               class="h-100 shadow-sm bg-light rounded-3 d-block p-4 text-center "
+            >
+               <h4 class="mb-3 h-32 package-name text-2xl my-auto items-center">{{ the_package.name }}</h4>
+               <h3 class="font-bold  flex gap-1 justify-center">
+                  <span class="text-2xl"> {{ the_package.price.toLocaleString() }} </span>
+                  <span class="text-2xl"> {{ $t('SAR','ريال') }} </span>
+               </h3>
+               <h1 class="w-100 my-6" style="border-bottom: 1px solid grey;"></h1>
+               <p class="text-2xl">{{ $t('Branches') }}</p>
+               <div class="mb-3 flex gap-1 justify-center flex-wrap text-center">
+                  <h1
+                     class="badge bg-cyan-500 choose text-center px-1  rounded-lg"
+                     v-for="branch in the_package.branches"
+                     :key="branch.name"
+                  >
+                     {{branch.name}}
+               </h1>
+               </div>
+               <div class="bg-cyan-400 rounded">
+                  <button
+                     class="btn btn-primary text-2xl choose "
+                     @click="viewPackage(the_package.id)"
+                  >
+                     {{ $t('Choose the package', 'اختيار الباقة') }}
+                  </button>
+               </div>
+            </div>
+         </div>
+         <!-- IF SMALL AND MEDIUM SCREEN -->
+         <div
+            class="col-sm-6 col-md-4 mb-4  rounded  lg:hidden  w-100 main-card"
+            v-for="the_package in packages?.data"
+            :key="the_package.id"
+         >
+            <div
+               class="h-100 shadow-sm bg-light rounded-3 d-block p-4 text-center "
             >
                <h4 class="mb-3 h-32 package-name text-2xl my-auto items-center">{{ the_package.name }}</h4>
                <h3 class="font-bold  flex gap-1 justify-center">
