d7e0b27f66494e154f22d0acc07c6c8bd73766f8
Mostafa Badr <mostafa_b2200@yahoo.com>
2024-05-27 09:44:16 +0300

    fix: payment failed page

--- pages/payment-failed/index.vue
+++ pages/payment-failed/index.vue
@@ -1,122 +1,92 @@
 <template>
    <section class="parent">
-      <CommonXfitLoader v-if="!success" />
-      <div v-if="success">
-         <h3 class="display-6 mb-5">
-            {{ $t("Thank you", "شكرا لك") }}
+      <CommonXfitLoader v-if="loading" />
+      <div v-else>
+         <h3 class="display-6 mb-5 text-2xl">
+            {{ $t("Sorry To You. Your payment was failed or cancelled", 'نأسف لك. تم فشل عملية الدفع او الغاءه' )}}
          </h3>
-         <p>
-            {{ $t("Payment completed successfully", "تمت عملية الدفع بنجاح") }}
-         </p>
-         <p>
+         <p class="text-gray-400">
             {{
                $t(
-                  "Enjoy achieving your goal with Xfit",
-                  "استمتع بتحقيق هدفك مع إكس فيت"
-               )
-            }}
-         </p>
-         <!-- <h1>hello {{ membershipId }}</h1> -->
-         <div v-for="membership in memberships?.data" :key="membership.id">
-            <ButtonsPrimary
-               v-if="membership.id === membershipId"
-               @click="viewMembership(membership.id)"
-               class="bg-cyan-500 choose cursor-pointer w-fit mt-6"
-            >
-               {{ $t("Membership Preview", "معاينة الإشتراك") }}
-            </ButtonsPrimary>
-         </div>
-      </div>
-      <div v-if="success == false">
-         <h3 class="display-6 mb-5">
-            {{ $t("Sorry To You", "نأسف لك") }}
-         </h3>
-         <p>
-            {{
-               $t(
-                  "Something went wrong, payment not completed",
-                  "حدث خطأ ما، ولم يكتمل الدفع"
-               )
-            }}
-         </p>
-         <p>
-            {{
-               $t(
-                  "try again later or use another payment method",
-                  "حاول مرة أخرى لاحقًا أو استخدم طريقة دفع أخرى"
+                  "Payment not completed. Please try again later or use another payment method",
+                  "لم تتم عملية الدفع. حاول مرة أخرى لاحقًا أو استخدم طريقة دفع أخرى"
                )
             }}
          </p>
          <!-- <h1>hello {{ membershipId }}</h1> -->
          <NuxtLink to="/packages">
-            <ButtonsPrimary
-               class="bg-cyan-500 choose cursor-pointer w-fit mt-6"
-            >
+            <ButtonsPrimary class="bg-cyan-500 choose cursor-pointer w-fit mt-6">
                {{ $t("Return to packages page", "العودة لصفحة الباقات") }}
-            </ButtonsPrimary></NuxtLink
-         >
+            </ButtonsPrimary>
+         </NuxtLink>
       </div>
    </section>
 </template>
 
 <script setup>
-import { viewMembership } from "@/utils/view/viewMembership";
-import { useGetMemberships } from "@/composables/memberships/useGetMemberships";
-onBeforeMount(() => getMemberships());
-const memberships = ref();
-
-const getMemberships = () => {
-   memberships.value = useGetMemberships();
-};
-
 import { useCustomAxios } from "~/composables/common/useCustomAxios";
-import { useGetMembership } from "@/composables/memberships/useGetMembership";
-const membership = ref();
-const getMembership = () => {
-   membership.value = useGetMembership();
-   if (!membership.value) {
-      return;
-   }
-};
+import { useGetTabbySession } from "@/composables/tabby/useGetTabbySession";
 
-const router = useRouter();
+const loading =ref(false);
+const route = useRoute();
+const order_id = route.query.orderId;
+const payment_status = route.query.paymentStatus;
+const charge_id = route.query.tap_id;
+const payment_id = route.query.payment_id;
 
-const order_id = router.currentRoute.value.query.orderId;
-const payment_status = router.currentRoute.value.query.paymentStatus;
-const charge_id = router.currentRoute.value.query.tap_id;
-const success = ref();
-
-const membershipId = ref();
 async function viewTapCharge() {
    try {
+      loading.value = true;
       const res = await useCustomAxios("memberships/view-tap-charge", {
          params: { id: charge_id },
       });
-      if (res.data.value.errors) {
-         throw res.data.value.errors;
-      }
-      success.value = res.data.value.success;
-      membershipId.value = res.data.value.membership_id;
    } catch (error) {
-      console.log(error?.response?.data?.message);
-      notify("danger", [
-         "Cant apply promo code.",
-         error?.response?.data?.message,
-      ]);
-   }
-}
+      console.log(error.response.data.message)
 
-async function viewTamaraSession() {
-   await useCustomAxios("memberships/view-tamara-session", {
-      params: { order_id: order_id, payment_status: payment_status },
-   });
+   } finally {
+      loading.value = false;
+   }
 }
 
 if (charge_id) {
    viewTapCharge();
 }
+
+async function viewTamaraSession() {
+   try {
+      loading.value = true;
+      const res = await useCustomAxios("memberships/view-tamara-session", {
+         params: { order_id: order_id, payment_status: payment_status },
+      });
+   } catch (err) {
+      console.log(err);
+   } finally {
+      loading.value = false;
+   }
+}
+
 if (order_id) {
    viewTamaraSession();
+}
+
+async function viewTabbySession() {
+
+   try {
+      loading.value = true;
+      await useGetTabbySession(
+         route.query.payment_id,
+         "Failed"
+      );
+     
+   } catch (err) {
+      console.log(err);
+   } finally {
+      loading.value = false;
+   }
+}
+
+if (payment_id) {
+   viewTabbySession();
 }
 </script>
 
