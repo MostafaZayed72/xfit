45263005d0a1a7d353417c4deb0e6c9c7a45078c
Mostafa Badr <mostafa_b2200@yahoo.com>
2024-05-27 09:44:07 +0300

    fix: payment completed page

--- pages/payment-completed/index.vue
+++ pages/payment-completed/index.vue
@@ -1,26 +1,19 @@
 <template>
    <section class="parent">
-      <CommonXfitLoader
-         v-if="
-            !success &&
-            successTamara != 'authorised' &&
-            successTabby != 'AUTHORIZED'
-         "
-      />
-      <div
-         v-if="
-            success ||
-            successTamara == 'authorised' ||
-            successTabby == 'AUTHORIZED'
-         "
-      >
-         <h3 class="display-6 mb-5">
+      <CommonXfitLoader v-if="loading" />
+      <!-- Success case -->
+      <div v-else-if="
+         success ||
+         successTamara == 'authorised' ||
+         successTabby == 'AUTHORIZED'
+      ">
+         <h3 class="display-6 mb-5 text-2xl">
             {{ $t("Thank you", "شكرا لك") }}
          </h3>
-         <p>
+         <p class="text-gray-400">
             {{ $t("Payment completed successfully", "تمت عملية الدفع بنجاح") }}
          </p>
-         <p>
+         <p class="text-gray-400">
             {{
                $t(
                   "Enjoy achieving your goal with Xfit",
@@ -30,18 +23,30 @@
          </p>
          <!-- <h1>hello {{ membershipId }}</h1> -->
          <div v-for="membership in memberships?.data" :key="membership.id">
-            <ButtonsPrimary
-               v-if="
-                  membership.id === membershipId ||
-                  membership.id == tamaraId ||
-                  membership.id == tabbyId
-               "
-               @click="viewMembership(membership.id)"
-               class="bg-cyan-500 choose cursor-pointer w-fit mt-6"
-            >
+            <ButtonsPrimary v-if="
+               membership.id === membershipId ||
+               membership.id == tamaraId ||
+               membership.id == tabbyId
+            " @click="viewMembership(membership.id)" class="bg-cyan-500 choose cursor-pointer w-fit mt-6">
                {{ $t("Membership Preview", "معاينة الإشتراك") }}
             </ButtonsPrimary>
          </div>
+      </div>
+      <!-- Failed case -->
+      <div v-else>
+         <h3 class="display-6 mb-5 text-2xl">
+            {{ $t("Sorry To You, Payment Failed or cancelled", "نأسف لك, فشللت عملية الدفع أو تم إلغائها") }}
+         </h3>
+         <p class="text-gray-400">
+            {{
+              failureReason
+            }}
+         </p>
+         <!-- <h1>hello {{ membershipId }}</h1> -->
+         <ButtonsPrimary class="bg-cyan-500 choose cursor-pointer w-fit mt-6"
+            @click="navigateTo('/member/memberships')">
+            {{ $t("Return to packages page", "العودة لصفحة الباقات") }}
+         </ButtonsPrimary>
       </div>
    </section>
 </template>
@@ -49,6 +54,8 @@
 <script setup>
 import { viewMembership } from "@/utils/view/viewMembership";
 import { useGetMemberships } from "@/composables/memberships/useGetMemberships";
+import { useGetTabbySession } from "@/composables/tabby/useGetTabbySession";
+
 onBeforeMount(() => getMemberships());
 const memberships = ref();
 
@@ -67,18 +74,27 @@ const getMembership = () => {
 };
 
 const router = useRouter();
+const route = useRoute();
+const loading = ref(false);
 
 const order_id = router.currentRoute.value.query.orderId;
 const payment_status = router.currentRoute.value.query.paymentStatus;
 const charge_id = router.currentRoute.value.query.tap_id;
+const payment_id = route.query.payment_id
 const success = ref();
+const failureReason = ref();
 const successTamara = ref();
 const successTabby = ref();
 const membershipId = ref();
 const tamaraId = ref();
 const tabbyId = ref();
+const record = ref(null);
+const membershipID = useState("membershipID");
+
+
 async function viewTapCharge() {
    try {
+      loading.value = true;
       const res = await useCustomAxios("memberships/view-tap-charge", {
          params: { id: charge_id },
       });
@@ -88,63 +104,77 @@ async function viewTapCharge() {
       success.value = res.data.value.success;
       membershipId.value = res.data.value.membership_id;
    } catch (error) {
-      console.log(error?.response?.data?.message);
-      notify("danger", [
-         "Cant apply promo code.",
-         error?.response?.data?.message,
-      ]);
+      console.log(error.response.data.message)
+      failureReason.value = error?.response?.data?.data?.response?.message || $t(
+         "Try again later or use another payment method",
+         "حاول مرة أخرى لاحقًا أو استخدم طريقة دفع أخرى"
+      );
+   } finally {
+      loading.value = false;
    }
 }
 
-async function viewTamaraSession() {
-   const res = await useCustomAxios("memberships/view-tamara-session", {
-      params: { order_id: order_id, payment_status: payment_status },
-   });
-   successTamara.value = res.data.value.payment_status;
-   console.log(res.data.value.membership_id);
-   tamaraId.value = res.data.value.membership_id;
-}
-import { useRoute } from 'vue-router';
-import { useGetTabbySession } from "@/composables/tabby/useGetTabbySession";
-import { useState } from '#app';
-
-const route = useRoute();
-const record = ref(null);
-const membershipID = useState("membershipID");
-
-async function viewTabbySession() {
-  if (route.query.payment_id) {
-    record.value = await useGetTabbySession(
-      route.query.payment_id,
-      "AUTHORIZED"
-    );
-
-    // تحقق إذا كانت الجلسة قد استرجعت بنجاح
-    if (record.value) {
-      // يمكنك الآن تنفيذ العمليات المطلوبة
-      console.log('Tabby session:', record.value.response.data);
-      successTabby.value = record.value.response.data.payment_status;
-tabbyId.value = record.value.response.data.membership_id;
-      
-    } else {
-      console.error('Failed to fetch Tabby session');
-    }
-  } else {
-    console.error('Payment ID is missing in the route query');
-  }
-}
-
-// استدعاء الدالة لعرض الجلسة
-viewTabbySession();
 if (charge_id) {
    viewTapCharge();
 }
+
+async function viewTamaraSession() {
+   try {
+      loading.value = true;
+      const res = await useCustomAxios("memberships/view-tamara-session", {
+         params: { order_id: order_id, payment_status: payment_status },
+      });
+      successTamara.value = res.data.value.payment_status;
+      console.log(res.data.value.membership_id);
+      tamaraId.value = res.data.value.membership_id;
+   } catch (err) {
+      console.log(err);
+      failureReason.value = err?.response?.data?.message || $t(
+         "Try again later or use another payment method",
+         "حاول مرة أخرى لاحقًا أو استخدم طريقة دفع أخرى"
+      );
+
+   } finally {
+      loading.value = false;
+   }
+}
+
 if (order_id) {
    viewTamaraSession();
 }
-// if (charge_id) {
-//    viewTabbySession();
-// }
+
+async function viewTabbySession() {
+   try {
+   loading.value = true;
+   if (route.query.payment_id) {
+      record.value = await useGetTabbySession(
+         route.query.payment_id,
+         "AUTHORIZED"
+      );
+
+
+      if (record.value) {
+         console.log('Tabby session:', record.value.response.data);
+         successTabby.value = record.value.response.data.payment_status;
+         tabbyId.value = record.value.response.data.membership_id;
+
+      } else {
+         console.error('Failed to fetch Tabby session');
+      }
+   } else {
+      console.error('Payment ID is missing in the route query');
+   }
+   } catch (err) {
+   console.log(err);
+   } finally {
+   loading.value = false;
+   }
+}
+
+if (payment_id) {
+   viewTabbySession();
+}
+
 </script>
 
 <style scoped>
